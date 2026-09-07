b885227580fcb2d3cb2fba0dbd06d93b92591e18
Posterminal <32669832+Posterminal@users.noreply.github.com>
2026-09-07 12:09:02 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,57 @@
-const CACHE_NAME = "schedule-4a-v5";
+const CACHE_NAME = "schedule-4a-v7";
 
 const FILES_TO_CACHE = [
   "./",
-  "./index.html
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.webmanifest",
+];
+
+/* Встановлення PWA */
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+
+  self.skipWaiting();
+});
+
+/* Видалення старих кешів */
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+
+  self.clients.claim();
+});
+
+/* Запити сайту */
+self.addEventListener("fetch", (event) => {
+  const url = new URL(event.request.url);
+
+  /*
+    NEPTUN ніколи не кешуємо.
+    Завжди отримуємо актуальний статус тривоги з інтернету.
+  */
+  if (url.origin === "https://neptun.in.ua") {
+    event.respondWith(fetch(event.request));
+    return;
+  }
+
+  /*
+    Для файлів самого сайту:
+    спершу кеш, якщо файлу немає — інтернет.
+  */
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
